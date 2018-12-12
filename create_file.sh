efebc83df363ed011ae912309639dbeea4da9ab4
satyam <satyamkumar4@acm.org>
2018-12-13 00:57:24 +0530

    Create_File: Check if a file is present in a directory if not than create one.
    Signed-off-by: Satyam Kumar<satyamkumar4@acm.org>

---
+++ create_file.sh
@@ -0,0 +1,24 @@
+# Take a string fromm user and check whether a file 
+# with same name is present in folder if not then create a 
+# a file with same name and write n nuber of lines in the 
+# newly created file(n will be entered by user).
+# Author: Satyam Kumar [satyamkumar4@acm.org]
+
+echo -n "ENter name of the file to be tested : "
+read fn
+if [ -f $fn ]
+then
+	echo "File ${fn} is present in the current directory"
+else
+	pwd=`pwd`
+	file=$pwd"/$fn"
+	echo -n "ENter number of lines to be added to file: "
+	read n
+	for((v=1;v<=$n;v++))
+	do
+		echo " This is line number ${v}" >> $file
+	done
+	echo $file
+	#cat $file
+	
+fi
